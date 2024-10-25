f7a9c927e4d83c09c9700b243603c5e944cf959b
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-25 18:26:09 +0400

    index page update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:5173",'https://fooddeliveryclient-q9ikzm0fn-sreelekshmi-unnikrishnans-projects.vercel'],
+  origin:["http://localhost:5173",'https://fooddeliveryclient-gxn2dz99w-sreelekshmi-unnikrishnans-projects.vercel.app'],
   methods: ['GET','POST','PUT','DELETE'],
   credentials:true
 }));
