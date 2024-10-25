cd30a165dbc2767193f6c5b4d978eaf4d5dfb73b
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-25 17:32:06 +0400

    bcrypt

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import { Admin } from "../models/adminModel.js";
 import { generateToken } from "../utilities/token.js";
--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import {sendRegistrationEmail} from '../utilities/nodemailer.js';
 import {generateToken} from '../utilities/token.js';
