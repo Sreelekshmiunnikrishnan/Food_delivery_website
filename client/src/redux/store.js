a9501945f62008bdae04e18ffafb0b87e5f3f58c
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-27 14:56:10 +0400

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "./features/userSlice";
+import AdminReducer from "../features/adminSlice";
 export const store = configureStore({
   reducer: {
     user: userReducer,
+},
+   reducer: {
+    admin: adminReducer,
 },
 })
