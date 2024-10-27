3cb11a7084e82c44a6313aa119157ccb991b25f5
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-27 16:47:44 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,24 +10,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:5173",'https://food-delivery-website-client.vercel.app'],
+  origin:["https://food-delivery-website-client.vercel.app","http://localhost:5173",],
   credentials:true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
 }));
 
-app.use((req, res, next) => {
-  const allowedOrigins = ['https://food-delivery-website-client.vercel.app']; // Add allowed origins here
-  const origin = req.headers.origin;
-  
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Credentials", "true");
-  }
-  
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 
 app.use(cookieParser());
