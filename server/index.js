6a5a65743353eba21e1e63d2f9d8fedc020101bc
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-27 14:07:01 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,16 +14,11 @@ app.use(cors({
   origin:["http://localhost:5173",'https://food-delivery-website-client.vercel.app'],
   methods: ['GET','POST','PUT','DELETE'],
   credentials:true,
-  allowedHeaders:['Content-type'],
+ 
 }));
 app.use(cookieParser());
 app.get('/', (req, res,next) => {
   res.send('Hello World!');
-  res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
  
 })
 
