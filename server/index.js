28173b70bdd895e72a20ad5e4b5174348885a734
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-27 23:41:53 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ const port = 3000
 connectDB();
 
 app.use(express.json());
-/* app.use(cors({
-  origin:["http://localhost:5173","https://food-delivery-website-client.vercel.app"],
+app.use(cors({
+  origin:["https://food-delivery-website-client.vercel.app"],
   
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
 }));
- */
-app.use(cors());
+ 
+
 
 
 app.use(cookieParser());
