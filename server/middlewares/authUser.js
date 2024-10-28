040cbdfe759083ed4a2259ebe0ba059bff5c735a
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-28 07:57:26 +0400

    Update authUser.js

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -4,17 +4,17 @@ export const authUser = (req,res,next) =>{
     try {
         const {token} = req.cookies;
         if(!token){
-            return res.status(401).json({message:'User not authorized',success:false});
+            return res.status(401).json({message:'User not authorized'});
        }
     const tokenVerified = jwt.verify(token,process.env.JWT_SECRET_KEY);
        if(!tokenVerified) {
-        return res.status(401).json({message:'User not authorized',success:false});
+        return res.status(401).json({message:'User not authorized'});
        }
        
        req.user=tokenVerified;
 
        next();
     } catch (error) {
-        return res.status(401).json({message:'user authorization failed',success:false});
+        return res.status(401).json({message:'user authorization failed'});
     }
 }
