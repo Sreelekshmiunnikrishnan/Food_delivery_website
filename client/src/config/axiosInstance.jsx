5b7dc94519419316f50ecb780b458903a5ed1c68
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-28 12:13:53 +0400

    Update axiosInstance.jsx

--- client/src/config/axiosInstance.jsx
+++ client/src/config/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-    baseURL:"https://foodorderwebsitedelicazy.netlify.app/api",
+    baseURL:`${API_URL}/api`,
     withCredentials: true,
 });
