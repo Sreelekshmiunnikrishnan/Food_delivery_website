19c62c256bb7ecbe4f00420f20cfc871ad7e2282
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-10-30 01:54:26 +0400

    index.js

--- server/index.js
+++ server/index.js
@@ -13,10 +13,9 @@ app.use(cors({
 
   origin:["http://localhost:5173",'https://foodorderwebsitedelicazy.netlify.app'],
 
- // origin: ['https://foodorderwebsitedelicazy.netlify.app',"http:localhost:5173"],
-
-  credentials:true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS']
+ credentials:true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: "Content-Type, Authorization"
    
 }));
  
