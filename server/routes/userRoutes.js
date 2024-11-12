362c60d7244215f50d2804b676849d738319092a
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-11-12 22:18:40 +0400

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = e.Router();
 import { register,login, checkUser,updateProfile,getProfile,deleteProfile,logout } from "../controllers/usercontrollers.js";
 import {authUser} from '../middlewares/authUser.js';
 import { getAllRestaurants, getRestaurant } from "../controllers/restaurantController.js";
-
+import { deleteReview } from "../controllers/reviewControllers.js";
 router.post("/signup",register)
 router.post("/login",login)
 router.put("/profile-update",authUser,updateProfile)
@@ -15,6 +15,7 @@ router.post("/logout",authUser,logout)
 router.get("/check-user",authUser,checkUser)
 router.get("getAllRestaurants",authUser,getAllRestaurants)
 router.get('/getRestaurant/:id',authUser,getRestaurant);
+router.delete("/deletereview/:id",authUser,deleteReview);
 //router.post('/add-to-cart',authUser,addToCart);
 //router.get('/getcart',authUser,getFromCart);
 //router.delete('/deletecart',authUser,removeFromCart);
